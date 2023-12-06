1bbe38a38ec2ace02eaa126dfed5658d13aca0e3
Schioppos <148727331+Schioppos@users.noreply.github.com>
2023-12-06 16:38:52 +0100

    esercizio6/12

--- script.js
+++ script.js
@@ -1,25 +1,44 @@
-let bottoneConserva = document.querySelector("#Conserva")
-let bottoneRimuovi = document.querySelector("#Rimuovi")
-let nome = document.querySelector("#nome")
+// script.js
 
- bottoneConserva.addEventListener("click", () => {
-    let valoreNome = nome.value
-    localStorage.setItem("valoreSalvato", valoreNome)
- })
-
-
- bottoneRimuovi.addEventListener("click", () => {
-    localStorage.removeItem("valoreSalvato")
- })
-
-
-let contatore = sessionStorage.getItem("contatore") || 0;
-
-function aggiornaContatore() {
-    contatore++;
-    sessionStorage.setItem("contatore", contatore);
-    document.getElementById("contatore").innerText = contatore;
+// Funzione per ottenere i libri dalla chiamata HTTP GET
+async function getBooks() {
+   try {
+       const response = await fetch('https://striveschool-api.herokuapp.com/books');
+       const books = await response.json();
+       displayBooks(books);
+   } catch (error) {
+       console.error('Errore nella chiamata HTTP:', error);
+   }
 }
 
-setInterval(aggiornaContatore, 1000);
+// Funzione per visualizzare i libri sulla pagina
+function displayBooks(books) {
+   const bookListContainer = document.getElementById('bookList');
 
+   books.forEach(book => {
+       const card = document.createElement('div');
+       card.classList.add('col-md-3', 'mb-4');
+
+       card.innerHTML = `
+           <div class="card">
+               <img src="${book.img}" class="card-img-top" alt="${book.title}">
+               <div class="card-body">
+                   <h5 class="card-title">${book.title}</h5>
+                   <p class="card-text">Prezzo: ${book.price} €</p>
+                   <button class="btn btn-danger" onclick="removeCard(this)">Scarta</button>
+               </div>
+           </div>
+       `;
+
+       bookListContainer.appendChild(card);
+   });
+}
+
+// Funzione per rimuovere una card quando viene premuto il pulsante "Scarta"
+function removeCard(button) {
+   const card = button.closest('.card');
+   card.remove();
+}
+
+// Richiama la funzione per ottenere i libri al caricamento della pagina
+getBooks();
